b2cb476958ba6bf6f6c2990bfbedc33ccf37691a
uottoni <uottoni@gmail.com>
2024-09-27 15:34:13 -0300

    Refactor Dockerfile and main.py for scheduled database backup

--- Dockerfile
+++ Dockerfile
@@ -3,9 +3,9 @@ FROM python:alpine3.16
 RUN apk add busybox-initscripts openrc --no-cache
 RUN pip install schedule
 WORKDIR /app
-COPY crontab /etc/cron.d/crontab
-COPY zabbix_os_sinal.py /app/zabbix_os_sinal.py
-RUN chmod 0644 /etc/cron.d/crontab
+#COPY crontab /etc/cron.d/crontab
+COPY main.py /app/main.py
+#RUN chmod 0644 /etc/cron.d/crontab
 
 #CMD ["/usr/bin/cron", "-l", "2" ,"-f", "/etc/cron.d/crontab"]
 #iniciar o script python a partir do venv
--- main.py
+++ main.py
@@ -6,12 +6,13 @@ import os
 import schedule
 import time
 
-HOST=os.getenv("HOST", 'cloud.rznet.com.br')
-PORT=os.getenv("PORT", '3369')
+DB_SERVER=os.getenv("HOST", 'cloud.rznet.com.br')
+DB_PORT=os.getenv("PORT", '3369')
+DB_NAMES = os.getenv("DB", 'solcontroldb_dev')
 DB_USER=os.getenv("DB_USER", 'root')
 DB_PASS=os.getenv("DB_PASS", '@@#123SqL')
 HORA_BKP = os.getenv("HORA_BKP", "00:00")
-DST_BKP = os.getenv("DST_BKP", "/tmp")
+DB_DUMP_TARGET = os.getenv("DST_BKP", "/tmp")
 
 # if using one database... ('database1',)
 databases=['solcontroldb_dev']
@@ -19,14 +20,17 @@ databases=['solcontroldb_dev']
 def get_dump(database):
     filestamp = time.strftime('%Y-%m-%d-%I')
     # D:/xampp/mysql/bin/mysqldump for xamp windows
-    os.popen("mysqldump -h %s -P %s -u %s -p%s %s > %s.sql" % (HOST,PORT,DB_USER,DB_PASS,database,DST_BKP+database+"_"+filestamp))
+    os.popen("mysqldump -h %s -P %s -u %s -p%s %s > %s.sql" % (DB_SERVER,DB_PORT,DB_USER,DB_PASS,database,DB_DUMP_TARGET+database+"_"+filestamp))
     
     print("\n|| Database dumped to "+database+"_"+filestamp+".sql || ")
 
 
 def rodar():
-    for database in databases:
-        get_dump(database)
+    #for database in databases:
+    get_dump(DB_NAMES)
 
 if __name__=="__main__":
     schedule.every().day.at(HORA_BKP).do(rodar)
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
