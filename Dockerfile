1c09edba7821d16b77c4d79b9ade2986c6407d58
uottoni <uottoni@gmail.com>
2024-10-01 14:24:06 -0300

    Refactor Dockerfile and main.py for scheduled database backup

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,7 @@
 FROM python:alpine3.16
-#RUN apt-get update && apt-get -y install cron vim
-RUN apk add busybox-initscripts openrc --no-cache
+RUN apk add busybox-initscripts openrc mariadb-client --no-cache
 RUN pip install schedule
 WORKDIR /app
-#COPY crontab /etc/cron.d/crontab
 COPY main.py /app/main.py
-#RUN chmod 0644 /etc/cron.d/crontab
-
-#CMD ["/usr/bin/cron", "-l", "2" ,"-f", "/etc/cron.d/crontab"]
 #iniciar o script python a partir do venv
-#CMD ["/bin/sh", "-c", "source .venv/scripts/activate && python3 -u ./zabbix_os_sinal.py"]
 CMD ["python3", "-u" ,"./main.py"]
--- main.py
+++ main.py
@@ -6,23 +6,24 @@ import os
 import schedule
 import time
 
-DB_SERVER=os.getenv("HOST", 'localhost')
-DB_PORT=os.getenv("PORT", '3306')
-DB_NAMES = os.getenv("DB", 'my_database')
+DB_SERVER=os.getenv("DB_SERVER", 'localhost')
+DB_PORT=os.getenv("DB_PORT", '3306')
+DB_NAMES = os.getenv("DB_NAMES", 'my_database')
 DB_USER=os.getenv("DB_USER", 'root')
 DB_PASS=os.getenv("DB_PASS", 'my_password')
 HORA_BKP = os.getenv("HORA_BKP", "00:00")
-DB_DUMP_TARGET = os.getenv("DST_BKP", "/tmp")
-
+DB_DUMP_TARGET = os.getenv("DB_DUMP_TARGET", "/tmp")
+RUN_ONCE=os.getenv("RUN_ONCE", 'false')
 # if using one database... ('database1',)
 
 
 def get_dump(database):
-    filestamp = time.strftime('%Y-%m-%d-%I')
+    filestamp = time.strftime('%Y-%m-%d_%I-%M-%S')
     # D:/xampp/mysql/bin/mysqldump for xamp windows
-    os.popen("mysqldump -h %s -P %s -u %s -p%s %s > %s.sql" % (DB_SERVER,DB_PORT,DB_USER,DB_PASS,database,DB_DUMP_TARGET+database+"_"+filestamp))
+    fileFullName = DB_DUMP_TARGET + '/' +database+"_"+filestamp+".sql"
+    os.popen("mysqldump -h %s -P %s -u %s -p%s %s > %s" % (DB_SERVER,DB_PORT,DB_USER,DB_PASS,database,fileFullName))
     
-    print("\n|| Database dumped to "+database+"_"+filestamp+".sql || ")
+    print("\n|| Database dumped to "+fileFullName+".sql || ")
 
 
 def rodar():
@@ -30,7 +31,10 @@ def rodar():
     get_dump(DB_NAMES)
 
 if __name__=="__main__":
-    schedule.every().day.at(HORA_BKP).do(rodar)
+    if(RUN_ONCE == 'true'):
+        schedule.every().minute.do(rodar)
+    else:
+        schedule.every().day.at(HORA_BKP).do(rodar)
     while True:
         schedule.run_pending()
         time.sleep(1)
